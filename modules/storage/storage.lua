04869a8bcab1cefcdfc66e28803a6e9ee5c2dc88
octacian <enduffy2014@outlook.com>
2017-02-22 17:16:09 -0800

    Rename 8k drive to 4k

--- modules/storage/storage.lua
+++ modules/storage/storage.lua
@@ -1,7 +1,7 @@
 -- microexpansion/storage.lua
 
--- [drive] 4k
-microexpansion.register_cell("cell_4k", {
-  description = "4k Storage Cell",
-  capacity = 4000,
+-- [drive] 8k
+microexpansion.register_cell("cell_8k", {
+  description = "8k Storage Cell",
+  capacity = 8000,
 })
