7245476030ec4caf5c48d7c4513934a91bb260c7
octacian <enduffy2014@outlook.com>
2017-02-24 14:42:52 -0800

    Power: Add super smelter
    Textures by @XtremeHacker
    Non-functional

--- modules/power/gen.lua
+++ modules/power/gen.lua
@@ -24,3 +24,26 @@ me.register_node("fuel_fired_generator", {
   groups = { cracky = 1 },
   paramtype2 = "facedir",
 })
+
+-- [register node] Super Smelter
+me.register_node("super_smelter", {
+  description = "Super Smelter",
+  tiles = {
+		"super_smelter_sides",
+		"super_smelter_sides",
+		"super_smelter_sides",
+		"super_smelter_sides",
+		"super_smelter_sides",
+		"super_smelter_front",
+	},
+  recipe = {
+    "single",
+    { 1,
+      { "default:furnace", "default:furnace", "default:furnace" },
+    	{ "default:steel_ingot", "microexpansion:machine_casing", "default:steel_ingot" },
+    	{ "default:steel_ingot", "default:steel_ingot", "default:steel_ingot" },
+    },
+  },
+  groups = { cracky = 1 },
+  paramtype2 = "facedir",
+})
