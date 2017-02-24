f4671fb68579596a687eefe9d3a12e4d285ea324
octacian <enduffy2014@outlook.com>
2017-02-24 14:14:41 -0800

    Add Shared Module
    Adds shared module along with Steel Infused Obsidian Ingot. Textures by @XtremeHacker

---
+++ modules/shared/init.lua
@@ -0,0 +1,17 @@
+-- shared/init.lua
+
+local me = microexpansion
+
+-- This mostly contains items that are used by multiple modules and
+-- don't really fit with anything else.
+
+-- [register item] Steel Infused Obsidian Ingot
+me.register_item("steel_infused_obsidian_ingot", {
+  description = "Steel Infused Obsidian Ingot",
+  recipe = {
+    "single",
+    { 1,
+      { "default:steel_ingot", "default:obsidian_shard", "default:steel_ingot" },
+    },
+  },
+})
