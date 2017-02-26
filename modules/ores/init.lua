d58afdee89b1bcf9906ec45692a18521bbbb4233
octacian <enduffy2014@outlook.com>
2017-02-26 10:54:15 -0800

    Ores: Fix code/module

--- modules/ores/init.lua
+++ modules/ores/init.lua
@@ -1,46 +1,44 @@
-minetest.register_node(microexpansion:incranium", {
+-- [register] Incranium Ore
+minetest.register_node("microexpansion:incranium", {
   description = "Incranium Ore",
-  tiles = incranium_ore.png" },
+  tiles = { "incranium_ore.png" },
   is_ground_content = true,
-  drop = microexpansion:incranium",
+  drop = "microexpansion:incranium",
   groups = { cracky=3, stone=1 },
 })
 
-
---Register Incranium Ore
+-- [ore] Register Incranium Ore
 minetest.register_ore({
   ore_type = "blob",
-  ore = microexpansion:incranium",
+  ore = "microexpansion:incranium",
   wherein = "default:stone",
   clust_scarcity = 4*4*4,
   clust_num_ores = 4,
   clust_size = 3,
   y_min = -300,
   y_max = -90,
-  --Oregen is very work in progress, and can & will be changed without prior warning
 })
 
 
---"Supernatet", pronounced "Super-nat-et" is Latin for "float", this ore will float up if there are no blocks above it, so be careful!
---Supernatet ore will be used to craft wings of flight
-minetest.register_node(microexpansion:incranium", {
+-- "Supernatet", pronounced "Super-nat-et" is Latin for "float", this ore will
+-- float up if there are no blocks above it, so be careful!
+-- Supernatet ore will be used to craft wings of flight
+minetest.register_node("microexpansion:supernatet", {
   description = "Supernatant Ore",
-  tiles = Supernatet_ore.png" },
+  tiles = { "supernatet_ore.png" },
   is_ground_content = true,
-  drop = microexpansion:incranium",
+  drop = "microexpansion:supernatet",
   groups = { cracky=3, stone=1 },
 })
 
-
-
+-- [ore] Register Supernatet Ore
 minetest.register_ore({
   ore_type = "blob",
-  ore = microexpansion:supernatet_ore",
+  ore = "microexpansion:supernatet_ore",
   wherein = "default:stone",
   clust_scarcity = 4*4*4,
   clust_num_ores = 4,
   clust_size = 3,
   y_min = -300,
   y_max = -90,
-  --Oregen is very work in progress, and can & will be changed without prior warning
 })
