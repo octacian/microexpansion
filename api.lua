307688744fd05ea177995ad6a13fa24cd8c045eb
DS-Minetest <vorunbekannt75@web.de>
2017-07-29 17:41:36 +0200

    replace space indents with tabs and co.

--- api.lua
+++ api.lua
@@ -3,150 +3,150 @@ local BASENAME = "microexpansion"
 
 -- [function] Register Recipe
 function microexpansion.register_recipe(output, recipe)
-  -- Check if disabled
-  if recipe.disabled == true then
-    return
-  end
+	-- Check if disabled
+	if recipe.disabled == true then
+		return
+	end
 
-  local function isint(n)
-    return n==math.floor(n)
-  end
+	local function isint(n)
+		return n==math.floor(n)
+	end
 
-  local function get_amount(_)
-    if isint(recipe[_][1]) then
-      return recipe[_][1]
-    else return 1 end
-  end
+	local function get_amount(_)
+		if isint(recipe[_][1]) then
+			return recipe[_][1]
+		else return 1 end
+	end
 
-  local function get_type(_)
-    if type(recipe[_][2]) == "string" then
-      return recipe[_][2]
-    end
-  end
+	local function get_type(_)
+		if type(recipe[_][2]) == "string" then
+			return recipe[_][2]
+		end
+	end
 
-  local function register(_)
-    local def = {
-      type   = get_type(_),
-      output = output.." "..tostring(get_amount(_)),
-      recipe = recipe[_][3] or recipe[_][2]
-    }
-    minetest.register_craft(def)
-  end
+	local function register(_)
+		local def = {
+			type   = get_type(_),
+			output = output.." "..tostring(get_amount(_)),
+			recipe = recipe[_][3] or recipe[_][2]
+		}
+		minetest.register_craft(def)
+	end
 
-  for _, i in ipairs(recipe) do
-    -- Check if disabled
-    if recipe.disabled == true then
-      return
-    end
+	for _, i in ipairs(recipe) do
+		-- Check if disabled
+		if recipe.disabled == true then
+			return
+		end
 
-    register(_)
-  end
+		register(_)
+	end
 end
 
 -- [function] Register oredef
 function microexpansion.register_oredef(ore, def)
-  -- Check if disabled
-  if def.disabled == true then
-    return
-  end
+	-- Check if disabled
+	if def.disabled == true then
+		return
+	end
 
-  local function register(_)
-    local def = def[_]
-    def.ore = "microexpansion:"..ore
-    minetest.register_ore(def)
-  end
+	local function register(_)
+		local def = def[_]
+		def.ore = "microexpansion:"..ore
+		minetest.register_ore(def)
+	end
 
-  for _, i in ipairs(def) do
-    -- Check if disabled
-    if def.disabled == true then
-      return
-    end
+	for _, i in ipairs(def) do
+		-- Check if disabled
+		if def.disabled == true then
+			return
+		end
 
-    register(_)
-  end
+		register(_)
+	end
 end
 
 -- [local function] Choose description colour
 local function desc_colour(status, desc)
-  if status == "unstable" then
-    return minetest.colorize("orange", desc)
-  elseif status == "no" then
-    return minetest.colorize("red", desc)
-  else
-    return minetest.colorize("white", desc)
-  end
+	if status == "unstable" then
+		return minetest.colorize("orange", desc)
+	elseif status == "no" then
+		return minetest.colorize("red", desc)
+	else
+		return minetest.colorize("white", desc)
+	end
 end
 
 -- [function] Register Item
 function microexpansion.register_item(itemstring, def)
-  -- Check if disabled
-  if def.disabled == true then
-    return
-  end
-  -- Set usedfor
-  if def.usedfor then
-    def.description = def.description .. "\n"..minetest.colorize("grey", def.usedfor)
-  end
-  -- Update inventory image
-  if def.inventory_image then
-    def.inventory_image = BASENAME.."_"..def.inventory_image..".png"
-  else
-    def.inventory_image = BASENAME.."_"..itemstring..".png"
-  end
-  -- Colour description
-  def.description = desc_colour(def.status, def.description)
+	-- Check if disabled
+	if def.disabled == true then
+		return
+	end
+	-- Set usedfor
+	if def.usedfor then
+		def.description = def.description .. "\n"..minetest.colorize("grey", def.usedfor)
+	end
+	-- Update inventory image
+	if def.inventory_image then
+		def.inventory_image = BASENAME.."_"..def.inventory_image..".png"
+	else
+		def.inventory_image = BASENAME.."_"..itemstring..".png"
+	end
+	-- Colour description
+	def.description = desc_colour(def.status, def.description)
 
-  -- Register craftitem
-  minetest.register_craftitem(BASENAME..":"..itemstring, def)
+	-- Register craftitem
+	minetest.register_craftitem(BASENAME..":"..itemstring, def)
 
-  -- if recipe, Register recipe
-  if def.recipe then
-    microexpansion.register_recipe(BASENAME..":"..itemstring, def.recipe)
-  end
+	-- if recipe, Register recipe
+	if def.recipe then
+		microexpansion.register_recipe(BASENAME..":"..itemstring, def.recipe)
+	end
 end
 
 -- [function] Register Node
 function microexpansion.register_node(itemstring, def)
-  -- Check if disabled
-  if def.disabled == true then
-    return
-  end
-  -- Set usedfor
-  if def.usedfor then
-    def.description = def.description .. "\n"..minetest.colorize("grey", def.usedfor)
-  end
-  -- Update texture
-  if auto_complete ~= false then
-    for _,i in ipairs(def.tiles) do
-      if #def.tiles[_]:split("^") <= 1 then
-        local prefix = ""
-        if def.type == "ore" then
-          prefix = "ore_"
-        end
+	-- Check if disabled
+	if def.disabled == true then
+		return
+	end
+	-- Set usedfor
+	if def.usedfor then
+		def.description = def.description .. "\n"..minetest.colorize("grey", def.usedfor)
+	end
+	-- Update texture
+	if auto_complete ~= false then
+		for _,i in ipairs(def.tiles) do
+			if #def.tiles[_]:split("^") <= 1 then
+				local prefix = ""
+				if def.type == "ore" then
+					prefix = "ore_"
+				end
 
-        def.tiles[_] = BASENAME.."_"..prefix..i..".png"
-      end
-    end
-  end
-  -- Colour description
-  def.description = desc_colour(def.status, def.description)
-  -- Update connect_sides
-  if def.connect_sides == "nobottom" then
-    def.connect_sides = { "top", "front", "left", "back", "right" }
-  elseif def.connect_sides == "machine" then
-    def.connect_sides = { "top", "bottom", "left", "back", "right" }
-  end
+				def.tiles[_] = BASENAME.."_"..prefix..i..".png"
+			end
+		end
+	end
+	-- Colour description
+	def.description = desc_colour(def.status, def.description)
+	-- Update connect_sides
+	if def.connect_sides == "nobottom" then
+		def.connect_sides = { "top", "front", "left", "back", "right" }
+	elseif def.connect_sides == "machine" then
+		def.connect_sides = { "top", "bottom", "left", "back", "right" }
+	end
 
-  -- Register node
-  minetest.register_node(BASENAME..":"..itemstring, def)
+	-- Register node
+	minetest.register_node(BASENAME..":"..itemstring, def)
 
-  -- if recipe, Register recipe
-  if def.recipe then
-    microexpansion.register_recipe(BASENAME..":"..itemstring, def.recipe)
-  end
+	-- if recipe, Register recipe
+	if def.recipe then
+		microexpansion.register_recipe(BASENAME..":"..itemstring, def.recipe)
+	end
 
-  -- if oredef, Register oredef
-  if def.oredef then
-    microexpansion.register_oredef(BASENAME..":"..itemstring, def.oredef)
-  end
+	-- if oredef, Register oredef
+	if def.oredef then
+		microexpansion.register_oredef(BASENAME..":"..itemstring, def.oredef)
+	end
 end
--- init.lua
+++ init.lua
@@ -9,9 +9,9 @@ microexpansion.gui_slots = "listcolors[#00000069;#5A5A5A;#141318;#30434C;#FFF]"
 
 -- logger
 function microexpansion.log(content, log_type)
-  if not content then return false end
-  if log_type == nil then log_type = "action" end
-  minetest.log(log_type, "[MicroExpansion] "..content)
+	if not content then return false end
+	if log_type == nil then log_type = "action" end
+	minetest.log(log_type, "[MicroExpansion] "..content)
 end
 
 -- Load API
@@ -27,40 +27,40 @@ local settings = Settings(modpath.."/modules.conf"):to_table()
 
 -- [function] Get module path
 function microexpansion.get_module_path(name)
-  local module_path = modpath.."/modules/"..name
+	local module_path = modpath.."/modules/"..name
 
-  if io.open(module_path.."/init.lua") then
-    return module_path
-  end
+	if io.open(module_path.."/init.lua") then
+		return module_path
+	end
 end
 
 -- [function] Load module (overrides modules.conf)
 function microexpansion.load_module(name)
-  if loaded_modules[name] ~= false then
-    local module_init = microexpansion.get_module_path(name).."/init.lua"
+	if loaded_modules[name] ~= false then
+		local module_init = microexpansion.get_module_path(name).."/init.lua"
 
-    if module_init then
-      dofile(module_init)
-      loaded_modules[name] = true
-      return true
-    else
-      microexpansion.log("Invalid module \""..name.."\". The module either does not exist "..
-        "or is missing an init.lua file.", "error")
-    end
-  else
-    return true
-  end
+		if module_init then
+			dofile(module_init)
+			loaded_modules[name] = true
+			return true
+		else
+			microexpansion.log("Invalid module \""..name.."\". The module either does not exist "..
+				"or is missing an init.lua file.", "error")
+		end
+	else
+		return true
+	end
 end
 
 -- [function] Require module (does not override modules.conf)
 function microexpansion.require_module(name)
-  if settings[name] and settings[name] ~= false then
-    return microexpansion.load_module(name)
-  end
+	if settings[name] and settings[name] ~= false then
+		return microexpansion.load_module(name)
+	end
 end
 
 for name,enabled in pairs(settings) do
-  if enabled ~= false then
-    microexpansion.load_module(name)
-  end
+	if enabled ~= false then
+		microexpansion.load_module(name)
+	end
 end
